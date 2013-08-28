195c5234fb7cc4a56a187298b6ab6feac2f2f7c4
Chase Arnold <chase4926@gmail.com>
2013-08-27 21:49:06 -0400

    Added hover buttons, and title

---
+++ skilltree.md
@@ -0,0 +1,23 @@
+For the purposes of the skill tree, 'x' refers to the current skill level and armor refers to the target's armor
+
+# Combat #
+  ## Tier 1 ##
+    ### Charge (5)###
+      Charges the enemy, having a (10x)% chance of stunning the enemy, preventing its next attack
+      and dealing ((power * x) - armor) damage
+    ### Bash (5)###
+      Bashes the enemy, attempting to overcome them with your armor. If your armor + (random range between -20% and 20% of armor) is greater than opponent's armor:
+        Stuns the enemy
+        Deals damage equal to the difference in armor values
+  ## Tier 2 ##
+  ## Tier 3 ##
+  ## Tier 4 ##
+  ## Tier 5 ##
+  ## Tier 6 ##
+# Naval #
+  ## Tier 1 ##
+  ## Tier 2 ##
+  ## Tier 3 ##
+  ## Tier 4 ##
+  ## Tier 5 ##
+  ## Tier 6 ##
--- src/menu.rb
+++ src/menu.rb
@@ -7,46 +7,81 @@ class Menu < ControllerObject
     super()
     @window = window
     @menu_xml = '../interface/menu.xml'
-    @bounding_box_manager = BoundingBoxManager.new()
+    @box_manager = BoundingBoxManager.new()
     register_buttons()
+    @current_boxes_under_mouse = Array.new()
+  end
+  
+  def update()
+    @current_boxes_under_mouse = @box_manager.hit_test_boxes(@window.relative_mouse_x, @window.relative_mouse_y)
   end
   
   def register_buttons()
-    @bounding_box_manager.register_box(XML_Manager['../interface/menu.xml']['new_game']['x'].to_i(),
-                                       XML_Manager['../interface/menu.xml']['new_game']['y'].to_i(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).width(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).height(),
-                                       'new_game')
-    @bounding_box_manager.register_box(XML_Manager['../interface/menu.xml']['load_game']['x'].to_i(),
-                                       XML_Manager['../interface/menu.xml']['load_game']['y'].to_i(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).width(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).height(),
-                                       'load_game')
-    @bounding_box_manager.register_box(XML_Manager['../interface/menu.xml']['options']['x'].to_i(),
-                                       XML_Manager['../interface/menu.xml']['options']['y'].to_i(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).width(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).height(),
-                                       'options')
-    @bounding_box_manager.register_box(XML_Manager['../interface/menu.xml']['quit']['x'].to_i(),
-                                       XML_Manager['../interface/menu.xml']['quit']['y'].to_i(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).width(),
-                                       Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).height(),
-                                       'quit')
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['new_game']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['new_game']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).height(),
+                              'new_game')
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['load_game']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['load_game']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).height(),
+                              'load_game')
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['options']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['options']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).height(),
+                              'options')
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['quit']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['quit']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).height(),
+                              'quit')
   end
   
   def draw()
+    #Title
+    Media::get_image(XML_Manager['../interface/menu.xml']['title']['image']).draw(XML_Manager['../interface/menu.xml']['title']['x'].to_i(), XML_Manager['../interface/menu.xml']['title']['y'].to_i(), 0)
     #New game button
-    box = @bounding_box_manager['new_game']
-    Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).draw(box.x, box.y, 1)
+    box = @box_manager['new_game']
+    if @current_boxes_under_mouse.include?('new_game') then
+      Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image_hover']).draw(box.x, box.y, 1)
+    else
+      Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).draw(box.x, box.y, 1)
+    end
     # Load game button
-    box = @bounding_box_manager['load_game']
-    Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).draw(box.x, box.y, 1)
+    box = @box_manager['load_game']
+    if @current_boxes_under_mouse.include?('load_game') then
+      Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image_hover']).draw(box.x, box.y, 1)
+    else
+      Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).draw(box.x, box.y, 1)
+    end
     # Options button
-    box = @bounding_box_manager['options']
-    Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).draw(box.x, box.y, 1)
+    box = @box_manager['options']
+    if @current_boxes_under_mouse.include?('options') then
+      Media::get_image(XML_Manager['../interface/menu.xml']['options']['image_hover']).draw(box.x, box.y, 1)
+    else
+      Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).draw(box.x, box.y, 1)
+    end
     # Quit button
-    box = @bounding_box_manager['quit']
-    Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).draw(box.x, box.y, 1)
+    box = @box_manager['quit']
+    if @current_boxes_under_mouse.include?('quit') then
+      Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image_hover']).draw(box.x, box.y, 1)
+    else
+      Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).draw(box.x, box.y, 1)
+    end
+  end
+  
+  def button_press_over_id(id, button_id)
+    case button_id
+      when Gosu::Button::MsLeft
+    end
+  end
+  
+  def button_down(id)
+    @current_boxes_under_mouse.each do |box_id|
+      button_press_over_id(box_id, id)
+    end
   end
 end
 
