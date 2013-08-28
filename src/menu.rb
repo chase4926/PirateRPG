0c80f94665f1903e53c3f85734bed250078a8e96
Chase Arnold <chase4926@gmail.com>
2013-08-28 18:16:15 -0400

    Added options background, moved images

--- src/menu.rb
+++ src/menu.rb
@@ -18,25 +18,25 @@ class Menu < ControllerObject
   end
   
   def register_buttons()
-    @box_manager.register_box(XML_Manager['../interface/menu.xml']['new_game']['x'].to_i(),
-                              XML_Manager['../interface/menu.xml']['new_game']['y'].to_i(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).width(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).height(),
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['main']['new_game']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['main']['new_game']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['new_game']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['new_game']['image']).height(),
                               'new_game')
-    @box_manager.register_box(XML_Manager['../interface/menu.xml']['load_game']['x'].to_i(),
-                              XML_Manager['../interface/menu.xml']['load_game']['y'].to_i(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).width(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).height(),
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['main']['load_game']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['main']['load_game']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['load_game']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['load_game']['image']).height(),
                               'load_game')
-    @box_manager.register_box(XML_Manager['../interface/menu.xml']['options']['x'].to_i(),
-                              XML_Manager['../interface/menu.xml']['options']['y'].to_i(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).width(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).height(),
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['main']['options']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['main']['options']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['options']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['options']['image']).height(),
                               'options')
-    @box_manager.register_box(XML_Manager['../interface/menu.xml']['quit']['x'].to_i(),
-                              XML_Manager['../interface/menu.xml']['quit']['y'].to_i(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).width(),
-                              Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).height(),
+    @box_manager.register_box(XML_Manager['../interface/menu.xml']['main']['quit']['x'].to_i(),
+                              XML_Manager['../interface/menu.xml']['main']['quit']['y'].to_i(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['quit']['image']).width(),
+                              Media::get_image(XML_Manager['../interface/menu.xml']['main']['quit']['image']).height(),
                               'quit')
   end
   
@@ -48,30 +48,34 @@ class Menu < ControllerObject
     #New game button
     box = @box_manager['new_game']
     if @current_boxes_under_mouse.include?('new_game') then
-      Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image_hover']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['new_game']['image_hover']).draw(box.x, box.y, 1)
     else
-      Media::get_image(XML_Manager['../interface/menu.xml']['new_game']['image']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['new_game']['image']).draw(box.x, box.y, 1)
     end
     # Load game button
     box = @box_manager['load_game']
     if @current_boxes_under_mouse.include?('load_game') then
-      Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image_hover']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['load_game']['image_hover']).draw(box.x, box.y, 1)
     else
-      Media::get_image(XML_Manager['../interface/menu.xml']['load_game']['image']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['load_game']['image']).draw(box.x, box.y, 1)
     end
     # Options button
     box = @box_manager['options']
     if @current_boxes_under_mouse.include?('options') then
-      Media::get_image(XML_Manager['../interface/menu.xml']['options']['image_hover']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['options']['image_hover']).draw(box.x, box.y, 1)
     else
-      Media::get_image(XML_Manager['../interface/menu.xml']['options']['image']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['options']['image']).draw(box.x, box.y, 1)
     end
     # Quit button
     box = @box_manager['quit']
     if @current_boxes_under_mouse.include?('quit') then
-      Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image_hover']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['quit']['image_hover']).draw(box.x, box.y, 1)
     else
-      Media::get_image(XML_Manager['../interface/menu.xml']['quit']['image']).draw(box.x, box.y, 1)
+      Media::get_image(XML_Manager['../interface/menu.xml']['main']['quit']['image']).draw(box.x, box.y, 1)
+    end
+    if @phase == 2 then
+      #Options Background
+      Media::get_image(XML_Manager['../interface/menu.xml']['options']['background']['image']).draw(XML_Manager['../interface/menu.xml']['options']['background']['x'].to_i(), XML_Manager['../interface/menu.xml']['options']['background']['y'].to_i(), 1)
     end
   end
   
