ee0de88d271ad5920d8312ba12c4c226d35de453
Chase Arnold <chase4926@gmail.com>
2013-09-04 14:47:52 -0400

    Continued work on battle system

--- src/enemy.rb
+++ src/enemy.rb
@@ -3,7 +3,7 @@
 
 
 class Enemy
-  attr_reader :stats, :battle_image
+  attr_accessor :stats, :battle_image
   def initialize()
     @stats = {'name'=> 'Captain Lechuck', 'vitality' => 5, 'precision' => 1, 'power' => 1, 'armor' => 1, 'health' => 50}
     @battle_image = Media::get_image('placeholder.png')
